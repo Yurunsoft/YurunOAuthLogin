51f68409548f02cbaa1116527e3ade32cb8b21f0
宇润 <admin@yurunsoft.com>
2017-08-08 15:44:49 +0800

    完成QQ登录改造

--- test/QQ/callback.php
+++ test/QQ/callback.php
@@ -1,7 +1,12 @@
 <?php
 require __DIR__ . '/common.php';
 $qqOAuth = new \Yurun\OAuthLogin\QQ\OAuth2($GLOBALS['oauth_qq']['appid'], $GLOBALS['oauth_qq']['appkey'], $GLOBALS['oauth_qq']['callbackUrl']);
-var_dump('access_token:', $qqOAuth->parseCallback($_SESSION['YURUN_QQ_STATE']), $qqOAuth->accessToken, $qqOAuth->result);
-var_dump('openid:', $qqOAuth->getOpenID(), $qqOAuth->openid, $qqOAuth->result);
-var_dump('info:', $qqOAuth->result);
-var_dump('userinfo:', $qqOAuth->getUserInfo());
+var_dump(
+	'access_token:', $qqOAuth->getAccessToken($_SESSION['YURUN_QQ_STATE']),
+	'我也是access_token:', $qqOAuth->accessToken,
+	'请求返回:', $qqOAuth->result
+);
+var_dump(
+	'用户资料:', $qqOAuth->getUserInfo(),
+	'openid:', $qqOAuth->openid
+);
