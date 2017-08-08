2ea2cd8037aed4e69ade72487f1d8cbcd43d8ecf
宇润 <admin@yurunsoft.com>
2017-08-08 17:03:15 +0800

    微博登录改造完成

--- test/Weibo/callback.php
+++ test/Weibo/callback.php
@@ -1,7 +1,12 @@
 <?php
 require __DIR__ . '/common.php';
 $weiboOAuth = new \Yurun\OAuthLogin\Weibo\OAuth2($GLOBALS['oauth_weibo']['appid'], $GLOBALS['oauth_weibo']['appkey'], $GLOBALS['oauth_weibo']['callbackUrl']);
-var_dump('access_token:', $weiboOAuth->parseCallback($_SESSION['YURUN_WEIBO_STATE']), $weiboOAuth->accessToken, $weiboOAuth->result);
-var_dump('openid:', $weiboOAuth->openid, $weiboOAuth->result);
-var_dump('info:', $weiboOAuth->result);
-var_dump('userinfo:', $weiboOAuth->getUserInfo());
+var_dump(
+	'access_token:', $weiboOAuth->getAccessToken($_SESSION['YURUN_WEIBO_STATE']),
+	'我也是access_token:', $weiboOAuth->accessToken,
+	'请求返回:', $weiboOAuth->result
+);
+var_dump(
+	'用户资料:', $weiboOAuth->getUserInfo(),
+	'openid:', $weiboOAuth->openid
+);
--- test/Weibo/login.php
+++ test/Weibo/login.php
@@ -2,14 +2,20 @@
 require __DIR__ . '/common.php';
 $weiboOAuth = new \Yurun\OAuthLogin\Weibo\OAuth2($GLOBALS['oauth_weibo']['appid'], $GLOBALS['oauth_weibo']['appkey'], $GLOBALS['oauth_weibo']['callbackUrl']);
 
+// 可选属性
+/*
+// 授权页面的终端类型，取值见微博文档。http://open.weibo.com/wiki/Oauth2/authorize
+$qqOAuth->display = null;
+// 是否强制用户重新登录，true：是，false：否。默认false。
+$qqOAuth->forcelogin = false;
+// 授权页语言，缺省为中文简体版，en为英文版。
+$qqOAuth->language = null;
+*/
 // 所有为null的可不传，这里为了演示和加注释就写了
 $url = $weiboOAuth->getAuthUrl(
-	$GLOBALS['oauth_weibo']['callbackUrl'],	// 回调地址，登录成功后返回该地址
-	null,									// $state 为空自动生成
-	null,									// $scope 为空默认
-	null,									// $display 为空默认
-	null,									// $forcelogin 为空默认
-	null									// $language 为空默认
+	null,	// 回调地址，登录成功后返回该地址
+	null,	// state 为空自动生成
+	null	// scope 只要登录默认为空即可
 );
 $_SESSION['YURUN_WEIBO_STATE'] = $weiboOAuth->state;
 header('location:' . $url);
