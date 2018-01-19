a31072120daa4f33406889170afea1185971fbae
宇润 <admin@yurunsoft.com>
2018-01-19 09:54:08 +0800

    新增百度登录授权

---
+++ test/Baidu/common.php
@@ -0,0 +1,7 @@
+<?php
+require dirname(__DIR__) . '/common.php';
+$GLOBALS['oauth_baidu'] = array(
+	'appid'			=>	'',
+	'appkey'		=>	'',
+	'callbackUrl'	=>	'http://test.com/test/Baidu/callback.php',
+);
---
+++ test/Baidu/loginAgent.php
@@ -0,0 +1,4 @@
+<?php
+require __DIR__ . '/common.php';
+$baiduOAuth = new \Yurun\OAuthLogin\Baidu\OAuth2;
+$baiduOAuth->displayLoginAgent();
