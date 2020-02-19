47b03232d5944e225cad92c8e4450615ebee780e
Yurun <admin@yurunsoft.com>
2020-02-19 11:59:42 +0800

    优化 QQ 获取 AccessToken 接口返回结果的判断逻辑

--- src/QQ/OAuth2.php
+++ src/QQ/OAuth2.php
@@ -76,14 +76,20 @@ class OAuth2 extends Base
 	 */
 	protected function __getAccessToken($storeState, $code = null, $state = null)
 	{
-		parse_str($this->http->get($this->getUrl('oauth2.0/token', array(
+		$content = $this->http->get($this->getUrl('oauth2.0/token', array(
 			'grant_type'	=>	'authorization_code',
 			'client_id'		=>	$this->appid,
 			'client_secret'	=>	$this->appSecret,
 			'code'			=>	isset($code) ? $code : (isset($_GET['code']) ? $_GET['code'] : ''),
 			'state'			=>	isset($state) ? $state : (isset($_GET['state']) ? $_GET['state'] : ''),
 			'redirect_uri'	=>	$this->getRedirectUri(),
-		)))->body(), $result);
+		)))->body();
+		$jsonData = json_decode($content, true);
+		if($jsonData)
+		{
+			throw new ApiException($jsonData['error_description'], $jsonData['error']);
+		}
+		parse_str($content, $result);
 		$this->result = $result;
 		if(isset($this->result['code']) && 0 != $this->result['code'])
 		{
