97cc107b290fabb6b24a5c42b83e5708684ddf7f
Yurun <admin@yurunsoft.com>
2020-03-12 18:53:56 +0800

    新增支持支付宝网页授权

---
+++ test/Alipay/callback.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/common.php';
+$alipayOAuth = new \Yurun\OAuthLogin\Alipay\OAuth2($GLOBALS['oauth_alipay']['appid'], $GLOBALS['oauth_alipay']['appkey'], $GLOBALS['oauth_alipay']['callbackUrl']);
+$alipayOAuth->appPrivateKey = $GLOBALS['oauth_alipay']['appPrivateKey'];
+// 解决只能设置一个回调域名的问题，下面地址需要改成你项目中的地址，可以参考./loginAgent.php写法
+// $alipayOAuth->loginAgentUrl = 'http://test.com/test/Alipay/loginAgent.php';
+var_dump(
+	'access_token:', $alipayOAuth->getAccessToken($_SESSION['YURUN_ALIPAY_STATE']),
+	'我也是access_token:', $alipayOAuth->accessToken,
+	'请求返回:', $alipayOAuth->result
+);
+var_dump(
+	'用户资料:', $alipayOAuth->getUserInfo(),
+	'openid:', $alipayOAuth->openid
+);
---
+++ test/Alipay/common.php
@@ -0,0 +1,10 @@
+<?php
+require dirname(__DIR__) . '/common.php';
+$GLOBALS['oauth_alipay'] = array(
+	'appid'			=>	'',
+	'appPrivateKey'	=>	<<<STR
+这里放应用私钥内容
+STR
+,
+	'callbackUrl'	=>	'http://test.com/test/Alipay/callback.php',
+);
