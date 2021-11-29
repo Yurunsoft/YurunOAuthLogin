118324cece07d46f760b976fe171a3bedd651e21
sy-records <52o@qq52o.cn>
2021-11-29 16:44:03 +0800

    Format code

--- test/GitLab/common.php
+++ test/GitLab/common.php
@@ -2,7 +2,7 @@
 
 require dirname(__DIR__) . '/common.php';
 $GLOBALS['oauth_gitlab'] = [
-    'appid'			     => '',
-    'appkey'		     => '',
-    'callbackUrl'	 => 'http://test.com/test/GitLab/callback.php',
+    'appid' => '',
+    'appkey' => '',
+    'callbackUrl' => 'http://test.com/test/GitLab/callback.php',
 ];
