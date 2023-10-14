46d6916eaaee39cd9893143e8562712344e2bd3f
Lu Fei <52o@qq52o.cn>
2023-10-14 20:35:12 +0800

    Update login.php

--- test/DingTalk/login.php
+++ test/DingTalk/login.php
@@ -4,7 +4,7 @@ require __DIR__ . '/common.php';
 $dingtalkOAuth = new \Yurun\OAuthLogin\DingTalk\OAuth2($GLOBALS['oauth_dingtalk']['appid'], $GLOBALS['oauth_dingtalk']['appkey'], $GLOBALS['oauth_dingtalk']['callbackUrl']);
 
 // 解决只能设置一个回调域名的问题，下面地址需要改成你项目中的地址，可以参考./loginAgent.php写法
- $dingtalkOAuth->loginAgentUrl = 'http://test.com/test/DingTalk/loginAgent.php';
+// $dingtalkOAuth->loginAgentUrl = 'http://test.com/test/DingTalk/loginAgent.php';
 
 // 所有为null的可不传，这里为了演示和加注释就写了
 $url = $dingtalkOAuth->getAuthUrl(
